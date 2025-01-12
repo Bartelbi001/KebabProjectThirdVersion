eab8a246b6de21f9e06c3d9ee1ac2b595f5de5f1
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-12 18:24:07 +0300

    feat: a new fields in BaseProductModel have been added

--- KebabProjectV3.Domain/Common/BaseProductModel.cs
+++ KebabProjectV3.Domain/Common/BaseProductModel.cs
@@ -1,8 +1,13 @@
-﻿namespace KebabProjectV3.Domain.Common;
+﻿using KebabProjectV3.Domain.Models;
+
+namespace KebabProjectV3.Domain.Common;
 
 public abstract class BaseProductModel
 {
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; }
+    public bool IsAvailable { get; set; }
+    public Image? TitleImage { get; set; }
 }
